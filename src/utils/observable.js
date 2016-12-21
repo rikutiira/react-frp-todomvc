6f569bef910b772d307b2cdd6420232b3520a255
Riku Tiira <riku@tiira.net>
2016-12-21 16:21:38 +0200

    Checked that emitter exists before calling it

--- src/utils/observable.js
+++ src/utils/observable.js
@@ -19,7 +19,11 @@ export const createAction = () => {
     let _emitter
 
     const stream = Kefir.stream((__emitter) => _emitter = __emitter)
-    const emitter = (...args) => _emitter.value(...args)
+    const emitter = (...args) => {
+        if (_emitter) {
+            _emitter.value(...args)
+        }
+    }
 
     return [ emitter, stream ]
 }
