fd91db6aa9488cb99b368603f3315e27d1f4e6f9
Riku Tiira <riku@tiira.net>
2016-12-23 16:34:34 +0200

    Added scss and autoprefixer support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: ['./src/index.js'],
@@ -7,6 +8,7 @@ module.exports = {
     progress: true,
     colors: true,
     watch: true,
+    devtool: 'source-map',
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'scripts/bundle.js',
@@ -24,9 +26,21 @@ module.exports = {
                 cacheDirectory: false
             },
         }, {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader'
+            test: /\.scss$/,
+            loader: `
+                style-loader!
+                css-loader?modules&localIdentName=[local]___[hash:base64:5]&sourceMap!
+                postcss-loader!
+                sass-loader?outputStyle=expanded&sourceMap!
+            `.replace(/\s/g, '')
         }]
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new HtmlWebpackPlugin()].concat(
+        process.env.NODE_ENV === 'production'
+            ? [new ExtractTextPlugin()]
+            : []
+    ),
+    sassLoader: {
+        includePaths: [path.join(__dirname, 'src', 'styles')]
+    }
 }
